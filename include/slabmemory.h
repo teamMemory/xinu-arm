fc672d1b1590e93330f53ed999ab5d69fd84488d
Blake Gross <Katerpilet@gmail.com>
2013-12-05 19:38:05 -0500

    Got new slabAlloc working

--- include/slabmemory.h
+++ include/slabmemory.h
@@ -1,6 +1,6 @@
 /**
  * @file slabmemory.h
- * @author Artur BRAGA
+ * @author Artur Braga; Blake Gross
  * Defintions for slab memory management system that wraps the kernel's malloc implementation.
  *
  */
@@ -12,9 +12,47 @@
 
 
 //typedef enum {false,true}bool;
-#define NBDATASTR 5
-#define NUM_ELEMENTS 100
-struct dataNode
+#define TOTAL_MEMORY 10240	// 10 kb
+
+// structure which will be used to keep track of free memory areas and occupied memory areas
+struct MemRange
+{
+	struct MemRange *pNext;
+	struct MemRange *pPrev;
+	uint NbPages;
+	struct Slab *pSlab;
+}
+// Cache Structure containing the list of slabs
+struct SlabCacheList
+{
+	struct SlabCacheList *pPrev;
+	struct SlabCacheList *pNext;
+	uint objSize;
+	uint allocSize;
+	uint slabObjCnt;
+	uint freeObj;
+	struct Slab *pSlabList;
+}
+//Slab structure containing the list of buffers
+struct Slab
+{
+	struct Slab *pPrev;
+	struct Slab *pNext;
+	uint NbFree;
+	struct BufferList *pFree;
+	void *firstObj; //memory address of the first element in the slab
+	struct MemRange *pRange;
+	struct SlabCacheList *pCache;
+}
+//List of actual objects contained in the Slab
+struct BufferList
+{
+	struct BufferList *pPrev;
+	struct BufferList *pNext;
+	void *pObject;
+}
+
+/*struct dataNode
 {
 	void* mem;
 	bool isTaken;
@@ -25,10 +63,13 @@ struct dataType
 {
 	uint size;
 	struct dataNode *memList;
-};
+};*/
 
 // accounting operations
 void slabInit(uint numEl);
+struct SlabCacheList *createCache(uint size);
+uint cacheDestroy(struct SlabCacheList *pCache);
+
 void* slabAlloc(uint elSize);
 void slabFree(void* base);
 
--- system/slabAllocator.c
+++ system/slabAllocator.c
@@ -1,6 +1,6 @@
 /**
  * @file slabAllocater.c
- * @author Artur BRAGA
+ * @author Artur Braga; Blake Gross
  * Manages the memory that is allocated
  *
  */
@@ -9,97 +9,67 @@
 #include <stdlib.h>
 
 
-struct dataType *dataCache;
+struct SlabCacheList *cacheHead;
+void* memBlock;
 
-void slabInit(uint numEl)
+void slabInit()
 {
-	dataCache = malloc(6 * sizeof(struct dataType));
-	dataCache[0].size=sizeof(uchar);
-	dataCache[0].memList=malloc(sizeof(struct dataNode));
-	dataCache[0].memList->mem=malloc(numEl*(sizeof(uchar)+1));
-	dataCache[1].size=sizeof(ushort);
-	dataCache[1].memList=malloc(sizeof(struct dataNode));
-	dataCache[1].memList->mem=malloc(numEl*(sizeof(ushort)+1));
-	dataCache[2].size=sizeof(uint);
-	dataCache[2].memList=malloc(sizeof(struct dataNode));
-	dataCache[2].memList->mem=malloc(numEl*(sizeof(uint)+1));
-	dataCache[3].size=sizeof(ulong);
-	dataCache[3].memList=malloc(sizeof(struct dataNode));
-	dataCache[3].memList->mem=malloc(numEl*(sizeof(ulong)+1));
-	dataCache[4].size=sizeof(bool);
-	dataCache[4].memList=malloc(sizeof(struct dataNode));
-	dataCache[4].memList->mem=malloc(numEl*(sizeof(bool)+1));
+	memBlock = malloc( TOTAL_MEMORY );
 }
 
 void* slabMalloc(uint elSize)
 {
 	int i,j;
-	if( dataCache == NULL )
+	struct SlabCacheList* cacheToUse = cacheHead;
+	while( cacheHead != NULL )
 	{
-		slabInit(NUM_ELEMENTS);	
+		if( cacheToUse->objSize == elSize )
+		{
+			break;
+		}
+		cacheHead = cacheHead->pNext;
 	}
 	
-	for( i = 0; i < NBDATASTR; ++i )		// for the amount of elements in the list...
+	if( cacheToUse == NULL )
 	{
-		if( dataCache[ i ].size == elSize)	// we look for the size that matches the requested allocation
+		cacheToUse = createCache(elSize);
+	}
+	
+	struct Slab* currentSlab = cacheHead->pSlabList;	// need to get way of getting this....
+	
+	
+	void* memoryToReturn = NULL;
+	while( currentSlab != NULL )
+	{
+		if( currentSlab-> NbFree > 0 )	// if the slab has free space, we can allocate in it
 		{
-			void* tempMemory = NULL;
-			struct dataNode *current = dataCache[i].memList;
-			do								// We go through the linked list until we find a "page" that is available for use
+			struct BufferList* headBuffer = currentSlab->pFree;
+			memoryToReturn = headBuffer->pObject;		// first object on list is always going to be free, now we have to take if off the list (works like a queue)
+
+			if( headBuffer->pNext != NULL )
 			{
-				if( !current->isTaken )
-				{
-					tempMemory = current->mem;
-					break;
-				}
-				
-				if( current->next != NULL )
-				{
-					current = current->next;
-				}
-				else
-				{
-					break;
-				}
-			}while( current->next != NULL );
+				headBuffer->pNext->pPrev = headBuffer->pPrev;	// reordering the list
+				headBuffer->pNext = NULL;
+				headBuffer->pPrev = NULL;
+			}
+			currentSlab->pFree = headBuffer->pNext;
 			
-			if( tempMemory != NULL )
-			{
-				for( j = 0; j < NUM_ELEMENTS; ++j )
-				{
-					bool* tempMemoryFull = (tempMemory + j * (elSize + 1) );
-					if( *tempMemoryFull == FALSE )
-					{
-						*tempMemoryFull = TRUE;
-						
-						// RETURN ALLOCATED MEMORY
-						return (void*)(&(*tempMemoryFull));
-					}
-				}
-				
-				current->isTaken = TRUE;	// went through whole "page" without finding anything
-				--i;						// resetting i, so that it will go through the search again
-				continue;
-			}
-			else							// No memory can be found so attempting to request more
-			{
-				current->next = malloc(sizeof(struct dataNode));
-				if( current->next != NULL )
-				{
-					current->next->mem = malloc((NUM_ELEMENTS*(elSize+1)));
-					if( current->next->mem != NULL )
-					{
-						--i;
-						continue;				// we want to retry the allocation now that we have expanded our memory
-					}
-				}
-				break;	// if the allocation failed, break out (drops to NULL return)
-				
-			}
+			--NbFree;
+			break;
 		}
+		
+		currentSlab = currentSlab->pNext;
 	}
 
 	// for now just returning rootnodes memory region
 	//return rootNode->memRegion;
-	return NULL;
+	return memoryToReturn;
+}
+
+struct SlabCacheList *createCache(uint size)
+{
+	if( memBlock == NULL )
+	{
+		slabInit();
+	}
 }
