3f03b772ea8dbd7bbd7f41eae1952815abfdc763
Paul T Aman <pta4540@cs.rit.edu>
2013-12-06 19:34:50 -0500

    added struct for frag

---
+++ include/fragStruct.h
@@ -0,0 +1,16 @@
+/*
+*	@file fragStruct.h
+*	@author Paul Aman
+*	struct representing amount of fragmentation occurring for a memory pool
+*/
+
+#ifndef _FRAGSTRUCT_H_
+#define _FRAGSTRUCT_H_
+
+struct MemFrag
+{
+	unsigned int intFrag;
+	unsigned int extFrag;
+	unsigned int memSize;
+};
+#endif //_FRAGSTRUCT_H_
